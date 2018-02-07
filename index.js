e68944c5771023645fb5487b6a3c0991c4e9b2f1
Zebulon Young <zebulonj@gmail.com>
2018-02-06 19:36:23 -0800

    Terminate only on type === 2 (ignore pull).

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function makeSubject() {
       const sink = data;
       sinks.push(sink);
       sink(0, t => {
-        const i = sinks.indexOf(sink);
-        if (i > -1) sinks.splice(i, 1);
+        if (t===2) {
+          const i = sinks.indexOf(sink);
+          if (i > -1) sinks.splice(i, 1);
+        }
       });
     } else {
       for (let i = 0, n = sinks.length; i < n; i++) {
